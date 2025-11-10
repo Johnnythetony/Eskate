8c271cd1817f8ffd7475e2e1fc31b5e85a0bc129
Johnnythetony <jamp1998@gmail.com>
2025-11-10 13:54:16 +0100

    Instalar react-hook-form y crear componente de formulario

--- app/index.tsx
+++ app/index.tsx
@@ -2,19 +2,19 @@ import { StyleSheet, Text, View } from "react-native";
 
 const Login = () => {
   return (
-    <View
-      style={styles.view}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  view: {
+  container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 20,
+    gap: 20
   }
 })
 
